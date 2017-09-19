19cd6e5580ccea2b49f39c956d0c9932b7ec364a
MacGregor <Andrew_MacGregor@student.uml.edu>
2017-09-19 12:52:38 -0400

    Servo Control changes to see the servo rotate timer code added tested Pic to led interface

---
+++ Lab1/Lab1_MPLABXproject/PIC16F18857_Starter_proj .X/helloWorld.c
@@ -0,0 +1,29 @@
+
+#include "mcc_generated_files/mcc.h" //default library 
+
+void main(void)
+{
+    // Initialize PIC device
+    SYSTEM_Initialize();
+
+    // Initialize the required modules 
+   
+    // **** write your code 
+    
+    
+
+    while (1) // keep your application in a loop
+    {
+        
+        
+        // ****** write your code 
+        
+        
+       
+        
+        
+        
+    }
+    
+    
+}
--- Lab1/Lab1_MPLABXproject/PIC16F18857_Starter_proj .X/main.c
+++ Lab1/Lab1_MPLABXproject/PIC16F18857_Starter_proj .X/main.c
@@ -11,28 +11,32 @@
 
 // ====================  prototype functions: ====================
 
-void Timer2_Init(void)  {}     
- /*  Configure Timer 2 and start it
+/* Configure Timer 2 and start it
+ */
+void Timer2_Init(void)  
+{
+ //* 1. Set the Basic Configuration Register for Timer 2 
+ //* 2. Set the Clock Source in relation with system Oscillator frequency 
+    T2CLKCON  = 0x06; //sets CS to SOSC freq (pg 440))
+ //* 3. T2PSYNC Not Synchronized; T2MODE Software control; T2CKPOL Rising Edge; T2CKSYNC Not Synchronized; Timer Mode
+    T2HLT = 0x00; //sets mode to free running with software gate.  (424))
+ //* 4. T2RSEL set reset source to Pin selected by T2INPPS (pg 443_)
+    T2RST = 0x00;
+ //* 5. Set PR2 255; 
+    PR2 = 255; //Timer2 Module Period Register
+ //* 6. Set TMR2  Prescale Value to 0 
+    T2CON |= 0x00; //Prescale is 0
+ //* 7. Clearing IF for timer 2
+    PIR4 &= 0x01; //clears only the Timer2 interrupt flag
+ //* 8. Start Timer2
+    T2CON |= 0x80;  //turn timer 2 on.
+}   
+
+
+
+void PWM_Init(void)  {
     
- USE Timer 2
- * 
- * ---- Set the Registers below:-----------
- * 
- * 1. Set the Basic Configuration Register for Timer 2 
- * 2. Set the Clock Source in relation with system Oscillator frequency 
- * 3. T2PSYNC Not Synchronized; T2MODE Software control; T2CKPOL Rising Edge; T2CKSYNC Not Synchronized; Timer Mode
- * 4. T2RSEL T2CKIPPS pin; Reset Source
- * 5. Set PR2 255; 
- * 6. Set TMR2  Prescale Value to 0 
- * 7. Clearing IF flag.
- * 8. Start Timer2
- * 
- * ----------------------
- */  
-
-
-
-void PWM_Init(void)  {}  
+}  
  /*  Configure CCP1 module in PWM mode (PWM channel = RC3 by default, Timer selection)
   Registers:
  * 
@@ -94,34 +98,103 @@ uint8_t ADC_conversion_results(unsigned ch) {}
 /*
 Develop your Application logic below
 */
+void servoRotate0() //0 Degree
+{
+  unsigned int i;
+  for(i=0;i<50;i++)
+  {
+    PORTB = 1;
+    __delay_ms(1.5);
+    PORTB = 0;
+    __delay_ms(18.5);
+  }
+}
 
+void servoRotate90() //90 Degree
+{
+  unsigned int i;
+  for(i=0;i<50;i++)
+  {
+    PORTB = 1;
+    __delay_ms(2);
+    PORTB = 0;
+    __delay_ms(18);
+  }
+}
+
+void servoRotate180() //-90 Degree
+{
+  unsigned int i;
+  for(i=0;i<50;i++)
+  {
+    PORTB = 1;
+    __delay_ms(1);
+    PORTB = 0;
+    __delay_ms(19);
+  }
+}
+void testMain(void)
+
+{
+  TRISB = 0; // PORTB as Ouput Port
+  do
+  {
+    servoRotate0(); //0 Degree
+    __delay_ms(2000);
+    servoRotate90(); //90 Degree
+    __delay_ms(2000);
+    servoRotate180(); //180 Degree
+  }while(1);
+    /*TRISA = 0x00; //all output
+
+    PORTA = 0x01; //start with it on
+
+    while(1)
+
+    {
+
+        __delay_ms(10);
+
+        PORTA = 0xFF; //toggle
+        
+        __delay_ms(10);
+        PORTA = 0x00; //off
+
+    }
+*/
+}
 void main(void)
 {
     // Initialize PIC device
     SYSTEM_Initialize();
 
     // Initialize the required modules 
-   
+    Timer2_Init(); //starts the timer.
+    ADC_Init();
+    PWM_Init();
     // **** write your code 
-    
-    
-
+       
+    bool servo_rotates_up = true;
     while (1) // keep your application in a loop
     {
+        //If the photoresister is above a certain threshold, turn on the LED (or not))
+        if(ADC_conversion_results(ADC_CHAN) > ADC_THRESH)
+            PORTA = 0x01;
+        else
+            PORTA = 0x00;
         
-        
-        // ****** write your code 
-        
-        
-        
-        // Debug your application code using the following statement
-        printf("Hello Lab 1 \n\r");
-        
-        
-        
-    }
-    
-    
+        //if the timer is done, rotate the servo the right direction
+        if(0 && servo_rotates_up/* && timer is triggered*/)
+        {
+            servoRotate180();
+            servo_rotates_up = false;
+        } 
+        else if(0 && !servo_rotates_up /** && timer is triggered*/)
+        {
+            servoRotate0();
+            servo_rotates_up = true;
+        }        
+    } 
 }
 
 /**
